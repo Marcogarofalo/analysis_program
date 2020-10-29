fd6d001dd8a808f3fa44127aaf7e864b070960b3
marco <marcogarofalo31@gmail.com>
2020-10-29 17:08:23 +0100

    new omega values

--- projects/clover/script_prace.sh
+++ projects/clover/script_prace.sh
@@ -10,9 +10,9 @@ make
 
 for e in $ensembles
 do
-#	t=`head -n 1 ../../$e/analysis/main/plateaux_masses.txt | awk '{print $1"   "$2}'`
-#        echo $e"    "  $t   	
-     ./form_factors read_plateaux -p ../../$e/analysis/main $sampling $pdf
+	t=`head -n 1 /media/marco/Elements/work/PRACE/$e/analysis/main/plateaux_masses.txt | awk '{print $1"   "$2}'`
+        echo $e"    "  $t   	
+#     ./form_factors read_plateaux -p ../../$e/analysis/main $sampling $pdf
  # ./petros_correlators read_plateaux -p ../../$e/analysis/main jack $pdf
 done
 #  ./form_factors_out_max_twist read_plateaux -p ../../beta1.726/cA211ab.12.48_no_rew/analysis/main $sampling $pdf
