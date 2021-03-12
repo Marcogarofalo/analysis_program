c5f0c30fd7b15e211ed44ff8061be722b5e04569
marco <marcogarofalo31@gmail.com>
2021-03-12 11:36:44 +0100

    try with this test

--- projects/phi4/test.cpp
+++ projects/phi4/test.cpp
@@ -99,8 +99,9 @@ int main(int argc, char **argv){
      int nerrors=0;
      for(int i=0;i< dataj[0].Nobs;i++){
         for(int j=0;j< dataj[0].Njack;j++){
-            double c=fabs( (dataj[0].jack[i][j]- dataj[1].jack[i][j] ) /dataj[0].jack[i][j]  );
-            if(c>1e-6) { printf("error at   obs=%d  jack=%d     ref=%g  test=%g \n",i,j,dataj[0].jack[i][j],dataj[1].jack[i][j]);  nerrors++; }
+            double err=error_jackboot("jack", dataj[0].Njack, dataj[0].jack[i]  );
+            double c=fabs( (dataj[0].jack[i][j]- dataj[1].jack[i][j] ) / err );
+            if(c>1e-4) { printf("error at   obs=%d  jack=%d     ref=%.10g  test=%.10g err=%.10g \n",i,j,dataj[0].jack[i][j],dataj[1].jack[i][j], err);  nerrors++; }
         }
      }
      if (nerrors>0){
