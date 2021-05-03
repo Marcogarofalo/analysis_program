5a19fd89818cfe8f70eed8d2015e0b3bb24a4710
marco <marcogarofalo31@gmail.com>
2021-05-03 12:41:35 +0200

    official rzeta as git submodule

--- modules/zeta/init_zeta_function.h
+++
@@ -1,4 +0,0 @@
-#ifndef _INIT_ZETA_FUNCTION_H
-#define _INIT_ZETA_FUNCTION_H
-int init_refrot_list (int rotref_list[48][2][3], int rotref_selection_permutation[4][4][48][3], int rotref_selection_sign[4][4][48][3], int rotref_number[4][4]);
-#endif
