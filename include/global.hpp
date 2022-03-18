aadc465a966f53a3223ab8878558b697c914ce15
marco <marcogarofalo31@gmail.com>
2022-03-18 21:58:17 +0100

    restore_default()

--- include/global.hpp
+++ include/global.hpp
@@ -62,296 +62,286 @@
 EXTERN int jack_tot;
 
 
-struct  header
-{
-    int twist;
-    int nf;
-    int nsrc,nsrcd;
-    int l0,l1,l2,l3;
-    int nk,nmoms;
-    double beta,ksea,musea,csw;
-    double *k,*mu,**mom;
-    
-    int allocated=0;
-} ;
- 
+struct  header {
+  int twist;
+  int nf;
+  int nsrc, nsrcd;
+  int l0, l1, l2, l3;
+  int nk, nmoms;
+  double beta, ksea, musea, csw;
+  double* k, * mu, ** mom;
 
-struct  kinematic
-{
-      double k2,k1,mom2,mom1,mom02,mom01;
-      int ik1,ik2;
-      double Mom1[4],Mom2[4];
-      int r2,r1;
-      int line;
-      char   plateau_m_ll[NAMESIZE];
-      char   plateau_m_sl[NAMESIZE];
-      char   plateau_f[NAMESIZE];
-      char   plateau_m_GEVP[NAMESIZE];
-} ;
-
-struct  kinematic_G
-{
-    double k0,kt,ks;
-    int ik0,ikt,iks;
-    double Mom0[4],Momt[4],Moms[4];
-    double p[4],k[4];
-    int rt,rs,i;
-    double  E_g,E_gT;
-    double kp;
-    int Twall;
-    const double eps1[4]={0,-1./1.41421356237,-1./1.41421356237,0};//1.41421356237=sqrt(2.)
-    const double eps2[4]={0,1./1.41421356237,-1./1.41421356237,0};
-    double eps1_curl_p[4],eps2_curl_p[4];
-    double eps1_curl_k[4],eps2_curl_k[4];
-    char plateau_RA[NAMESIZE];
-    char plateau_RV[NAMESIZE];
-    char plateau_H_H0_A[NAMESIZE];
-    
-} ;
+  int allocated = 0;
+};
 
 
-struct  database_file_jack
-{
-    char M_PS_GEVP[NAMESIZE];
-    char M_PS[NAMESIZE];
-    char f_PS[NAMESIZE];
-    char Zf_PS[NAMESIZE];
-    char f_PS_ls_ss[NAMESIZE];
-    char FV[NAMESIZE],FV_autoplateaux[NAMESIZE];
-    char FAp1[NAMESIZE];
-    char FA[NAMESIZE],FA_autoplateaux[NAMESIZE];
-    char FAp[NAMESIZE];
-    char xG[NAMESIZE];
-    char FV_exclude[NAMESIZE];
-    char FAp1_exclude[NAMESIZE];
-    char kp[NAMESIZE];
-    char mpcac[NAMESIZE];
-    char sampling[10];
+struct  kinematic {
+  double k2, k1, mom2, mom1, mom02, mom01;
+  int ik1, ik2;
+  double Mom1[4], Mom2[4];
+  int r2, r1;
+  int line;
+  char   plateau_m_ll[NAMESIZE];
+  char   plateau_m_sl[NAMESIZE];
+  char   plateau_f[NAMESIZE];
+  char   plateau_m_GEVP[NAMESIZE];
+};
 
-    int Njack;    
-    double a;
-    FILE *f_M_PS;
-    FILE *f_f_PS,*f_Zf_PS;
-    FILE *f_M_PS_GEVP;
-    FILE *f_f_PS_ls_ss;
-    FILE *f_FAp, *f_FA, *f_FA_autoplateaux;
-    FILE *f_FV,*f_FV_autoplateaux;
-    FILE *f_xG;
-    FILE *f_mpcac;
-    
-    char FA_from_H0[NAMESIZE];
-    char FA_from_H0_autoplateaux[NAMESIZE];
-    char FV_from_H0[NAMESIZE];
-    char FV_from_H0_autoplateaux[NAMESIZE];
-    char FV_from_H0_HA[NAMESIZE];
+struct  kinematic_G {
+  double k0, kt, ks;
+  int ik0, ikt, iks;
+  double Mom0[4], Momt[4], Moms[4];
+  double p[4], k[4];
+  int rt, rs, i;
+  double  E_g, E_gT;
+  double kp;
+  int Twall;
+  const double eps1[4] = { 0,-1. / 1.41421356237,-1. / 1.41421356237,0 };//1.41421356237=sqrt(2.)
+  const double eps2[4] = { 0,1. / 1.41421356237,-1. / 1.41421356237,0 };
+  double eps1_curl_p[4], eps2_curl_p[4];
+  double eps1_curl_k[4], eps2_curl_k[4];
+  char plateau_RA[NAMESIZE];
+  char plateau_RV[NAMESIZE];
+  char plateau_H_H0_A[NAMESIZE];
 
 };
 
 
-struct  data_jack
-{
-    ///dataJ[ensemble].observable[mass_index][jack]
-    double **M_PS_GEVP_jack,**f_PS_jack;
-    double **M_PS_jack,**f_PS_ls_ss_jack;
-    double **M_Omega_jack;
-    
-     ///dataJ[ensemble].observable[jack]
-    double *w0,*Zp,*Zv;
-    double *scalefm;
-    double *s0_w0;
-    double *s0_w0_cont;
-    
-    
-     ///dataJ[ensemble].observable[mass_index]
-    double *KM,*Kf;
+struct  database_file_jack {
+  char M_PS_GEVP[NAMESIZE];
+  char M_PS[NAMESIZE];
+  char f_PS[NAMESIZE];
+  char Zf_PS[NAMESIZE];
+  char f_PS_ls_ss[NAMESIZE];
+  char FV[NAMESIZE], FV_autoplateaux[NAMESIZE];
+  char FAp1[NAMESIZE];
+  char FA[NAMESIZE], FA_autoplateaux[NAMESIZE];
+  char FAp[NAMESIZE];
+  char xG[NAMESIZE];
+  char FV_exclude[NAMESIZE];
+  char FAp1_exclude[NAMESIZE];
+  char kp[NAMESIZE];
+  char mpcac[NAMESIZE];
+  char sampling[10];
+
+  int Njack;
+  double a;
+  FILE* f_M_PS;
+  FILE* f_f_PS, * f_Zf_PS;
+  FILE* f_M_PS_GEVP;
+  FILE* f_f_PS_ls_ss;
+  FILE* f_FAp, * f_FA, * f_FA_autoplateaux;
+  FILE* f_FV, * f_FV_autoplateaux;
+  FILE* f_xG;
+  FILE* f_mpcac;
+
+  char FA_from_H0[NAMESIZE];
+  char FA_from_H0_autoplateaux[NAMESIZE];
+  char FV_from_H0[NAMESIZE];
+  char FV_from_H0_autoplateaux[NAMESIZE];
+  char FV_from_H0_HA[NAMESIZE];
+
 };
 
 
-struct  result_jack
-{
-    ///dataJ[ensemble].observable[mass_index][jack]
-    double *Bw,*fw,*l3b,*l4b;
-    double *Bw_from_M,*fw_from_M,*l4b_from_M;
-    
-     ///dataJ[ensemble].observable[jack]
-    double *mlw,*fpiw ;
-    double *fpiw_from_M,*fKw_from_M,*fDw_from_M,*fDsw_from_M,*fDs_fD_from_M;
-    
-    double  *msw,*fkw,*ms_mud,*fk_fpi,*fk_fpi_from_M;
-    double *mcw,*fDw,*fDsw,*mc_ms;
-    
-    double  *w0fm, *w0MeV,*MpiMeV,*MKMeV,*MDMeV,*MDsMeV;
-    double **PMPi,***PMK,***PMD;
-    
-    double *fpiMeV_exp;
-    double *fKMeV_exp;
-    
-    double *MOmegaMeV;
-    
+struct  data_jack {
+  ///dataJ[ensemble].observable[mass_index][jack]
+  double** M_PS_GEVP_jack, ** f_PS_jack;
+  double** M_PS_jack, ** f_PS_ls_ss_jack;
+  double** M_Omega_jack;
+
+  ///dataJ[ensemble].observable[jack]
+  double* w0, * Zp, * Zv;
+  double* scalefm;
+  double* s0_w0;
+  double* s0_w0_cont;
+
+
+  ///dataJ[ensemble].observable[mass_index]
+  double* KM, * Kf;
+};
+
+
+struct  result_jack {
+  ///dataJ[ensemble].observable[mass_index][jack]
+  double* Bw, * fw, * l3b, * l4b;
+  double* Bw_from_M, * fw_from_M, * l4b_from_M;
+
+  ///dataJ[ensemble].observable[jack]
+  double* mlw, * fpiw;
+  double* fpiw_from_M, * fKw_from_M, * fDw_from_M, * fDsw_from_M, * fDs_fD_from_M;
+
+  double* msw, * fkw, * ms_mud, * fk_fpi, * fk_fpi_from_M;
+  double* mcw, * fDw, * fDsw, * mc_ms;
+
+  double* w0fm, * w0MeV, * MpiMeV, * MKMeV, * MDMeV, * MDsMeV;
+  double** PMPi, *** PMK, *** PMD;
+
+  double* fpiMeV_exp;
+  double* fKMeV_exp;
+
+  double* MOmegaMeV;
+
 };
 
 
 
 
-struct  jack_fit
-{
-  double **m,**oPp;
-  double **RA,**RV,**HA,**HV,**RA_autoplateaux,**RV_autoplateaux;
-  double **f_PS,**Zf_PS;  
-    
-};
-struct  fit_type
-{
-  double (*function)(int,int,double*,int,double*);//N, Nvar, x ,Npar,P
-  double (*f1)(int,int,double*,int,double*);
-  double (*f2)(int,int,double*,int,double*);
-  double **ext_P;  //parameter to not fit, they will be add to the Nvar,  Nvar=variables+(prameter to not fit)
+struct  jack_fit {
+  double** m, ** oPp;
+  double** RA, ** RV, ** HA, ** HV, ** RA_autoplateaux, ** RV_autoplateaux;
+  double** f_PS, ** Zf_PS;
 
-  int N,Npar,Nvar,Njack;  
+};
+struct  fit_type {
+  double (*function)(int, int, double*, int, double*);//N, Nvar, x ,Npar,P
+  double (*f1)(int, int, double*, int, double*);
+  double (*f2)(int, int, double*, int, double*);
+  double** ext_P;  //parameter to not fit, they will be add to the Nvar,  Nvar=variables+(prameter to not fit)
+
+  int N, Npar, Nvar, Njack;
   int n;// an index to be passed to (*function)( iN ,int,double*,int,double*)
-  
-  int n_ext_P=0; //number of external parameter that will no be fitted
-  int custom=false; // false=0 means default fit , 1 custom fit options
-  double lambda=0.001;
-  double acc=0.001; 
-  double h=1e-5; 
-  int devorder=4;  // 2 , 4 , -2 adaptive h=h*parameter
-  int repeat_start=1;
-  double chi2_gap_jackboot=1;
-  int guess_per_jack=3;
-  int precision_sum=0;// 0 float, 1 float kahan,  >1 long double
-  bool mean_only=false;
-  
-  
-  std::vector<int> corr_id={};
-  std::vector<double>  Prange={};
-  std::vector<double>  guess={};
+
+  int n_ext_P = 0; //number of external parameter that will no be fitted
+  int custom = false; // false=0 means default fit , 1 custom fit options
+  double lambda = 0.001;
+  double acc = 0.001;
+  double h = 1e-5;
+  int devorder = 4;  // 2 , 4 , -2 adaptive h=h*parameter
+  int repeat_start = 1;
+  double chi2_gap_jackboot = 1;
+  int guess_per_jack = 3;
+  int precision_sum = 0;// 0 float, 1 float kahan,  >1 long double
+  bool mean_only = false;
+
+
+  std::vector<int> corr_id = {};
+  std::vector<double>  Prange = {};
+  std::vector<double>  guess = {};
 
   // GEVP
-  int t0_GEVP=3;
-  int value_or_vector=0;  // eigenvalues=0 or eigenvalues =1
-  bool GEVP_tpt0=false;
-  bool GEVP_swap_t_t0=false;
-  int GEVP_ignore_warning_after_t=1000;
-  
+  int t0_GEVP = 3;
+  int value_or_vector = 0;  // eigenvalues=0 or eigenvalues =1
+  bool GEVP_tpt0 = false;
+  bool GEVP_swap_t_t0 = false;
+  int GEVP_ignore_warning_after_t = 1000;
+
   // HANKEL
-  int HENKEL_size=1;
+  int HENKEL_size = 1;
 
   int T;
   int L;
-  bool plateaux_scan=false;
-  FILE *f_plateaux_scan=NULL;
+  bool plateaux_scan = false;
+  FILE* f_plateaux_scan = NULL;
   std::string name_plateaux_scan;
-  
+
   std::string resampling;
-  int verbosity=0;
-  
-  void restore_default(){
-     custom=false; // 1 means default fit , 0 custom fit options
-     lambda=0.001;
-     acc=0.001; 
-     h=1e-5; 
-     Prange=std::vector<double>();
-     guess=std::vector<double>();
-     corr_id=std::vector<int>();
-     devorder=4; 
-     repeat_start=1;
+  int verbosity = 0;
 
-     mean_only=false;
-     
-     plateaux_scan=false;
-     guess_per_jack=0;
-     chi2_gap_jackboot=1;
-     
-     precision_sum=0;
-     verbosity=0;
-     
-     
-     t0_GEVP=3;
-     value_or_vector=0;
-     GEVP_tpt0=false;
-     GEVP_swap_t_t0=false;
-     GEVP_ignore_warning_after_t=1000;
+  void restore_default() {
+    custom = false; // 1 means default fit , 0 custom fit options
+    lambda = 0.001;
+    acc = 0.001;
+    h = 1e-5;
+    Prange = std::vector<double>();
+    guess = std::vector<double>();
+    corr_id = std::vector<int>();
+    devorder = 4;
+    repeat_start = 1;
 
-     HENKEL_size=1;
+    mean_only = false;
 
-     for (int i=0;i<n_ext_P;i++){
-       ext_P[i]=nullptr;
-     }
-     free(ext_P);
-     n_ext_P=0;
-//      f_plateaux_scan=NULL;
-//      name_plateaux_scan="\0";
+    plateaux_scan = false;
+    guess_per_jack = 0;
+    chi2_gap_jackboot = 1;
+
+    precision_sum = 0;
+    verbosity = 0;
+
+
+    t0_GEVP = 3;
+    value_or_vector = 0;
+    GEVP_tpt0 = false;
+    GEVP_swap_t_t0 = false;
+    GEVP_ignore_warning_after_t = 1000;
+
+    HENKEL_size = 1;
+    if (n_ext_P > 0) {
+      for (int i = 0;i < n_ext_P;i++) {
+        ext_P[i] = nullptr;
+      }
+      free(ext_P);
+      n_ext_P = 0;
+    }
+    //      f_plateaux_scan=NULL;
+    //      name_plateaux_scan="\0";
   };
-  
-  
+
+
 };
 
-struct fit_result
-{
+struct fit_result {
   int Njack;
-  double **P;// [par][jack]
-  double *chi2;
-  double ***C;   
-    
+  double** P;// [par][jack]
+  double* chi2;
+  double*** C;
+
 };
 
 
-struct fit_all
-{
-    int Nfits;
-    struct fit_type  *info;
-    struct fit_result  *out;
+struct fit_all {
+  int Nfits;
+  struct fit_type* info;
+  struct fit_result* out;
 };
 
-struct observable
-{
-   char name_out[NAMESIZE]; 
-   FILE *f_out;
-   
-   char name_plateaux[NAMESIZE];
-   FILE *f_plateaux;
-   
-   char name_jack[NAMESIZE];
-   FILE *f_jack;
-   
-    
+struct observable {
+  char name_out[NAMESIZE];
+  FILE* f_out;
+
+  char name_plateaux[NAMESIZE];
+  FILE* f_plateaux;
+
+  char name_jack[NAMESIZE];
+  FILE* f_jack;
+
+
 };
 
 
-struct store_fit_clover{
-    std::string M;
-    std::string GF;
-    double *jack_m;
-    double *jack_f;
-    double *jack_B;
-    double *chi2;
-    double *Sigma_13;
-    
-    double *ms;
-    double *ms_mud;
-    double *fk;
-    double *fk_fpi;
-    
-    double *mc;
-    double *mc_ms;
-    double *fD;
-    double *fD_fk;
-    
-    double *fDs;
-    double *fDs_fD;
-    
-    double *w0;
-    
-    int Njack;
-    std::string name;
+struct store_fit_clover {
+  std::string M;
+  std::string GF;
+  double* jack_m;
+  double* jack_f;
+  double* jack_B;
+  double* chi2;
+  double* Sigma_13;
+
+  double* ms;
+  double* ms_mud;
+  double* fk;
+  double* fk_fpi;
+
+  double* mc;
+  double* mc_ms;
+  double* fD;
+  double* fD_fk;
+
+  double* fDs;
+  double* fDs_fD;
+
+  double* w0;
+
+  int Njack;
+  std::string name;
 };
 
 EXTERN struct header file_head;
 EXTERN struct database_file_jack file_jack;//used to save the jack
-EXTERN struct data_jack *dataJ,*gjack;
+EXTERN struct data_jack* dataJ, * gjack;
 EXTERN struct result_jack result;
-EXTERN int **index_a;
+EXTERN int** index_a;
 EXTERN int corr_counter;
 EXTERN fit_type default_fit_info;
 #endif
