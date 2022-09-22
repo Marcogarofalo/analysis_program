3c7c388cd00a5adcdd50e2e1ed7f65f8207f3c2c
Marcogarofalo <marcogarofalo31@gmail.com>
2022-09-22 15:29:59 +0200

    project template

---
+++ projects/project_template/functions_project_template.cpp
@@ -0,0 +1,8 @@
+#define functions_project_template_C
+#include "functions_project_template.hpp"
+
+double lhs_function_project_template_eg(int j, double**** in, int t, struct fit_type fit_info){
+    double r=in[j][0][t][0];
+    return r;
+}
+
---
+++ projects/project_template/functions_project_template.hpp
@@ -0,0 +1,7 @@
+#ifndef functions_project_template_H
+#define functions_project_template_H
+#include "non_linear_fit.hpp"
+
+double lhs_function_project_template_eg(int j, double**** in, int t, struct fit_type fit_info);
+
+#endif
