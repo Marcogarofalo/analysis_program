c675c9d5144831cbb86ca06a86cd4227225234da
Marcogarofalo <marcogarofalo31@gmail.com>
2023-03-20 16:47:46 +0100

    reset pointer after the header

--- modules/fit_all.cpp
+++ modules/fit_all.cpp
@@ -130,8 +130,8 @@ void generic_header::read_header(FILE* file) {
     int confs = tmp / (sizeof(int) + (size) * sizeof(double));
     error(confs != Njack, 1, "generic_header::read_header",
         "size of the data chunk is not equal to ncorr*2*T\n");
-
-
+    fseek(file, tmp, SEEK_SET);
+    
 }
 
 
