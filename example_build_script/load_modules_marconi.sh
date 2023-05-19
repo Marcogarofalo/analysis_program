b2fee93ae3660559f31a4a46dbc3fbc44662a2c6
Marcogarofalo <marcogarofalo31@gmail.com>
2023-05-19 14:12:08 +0200

    marconi compilation

---
+++ example_build_script/load_modules_marconi.sh
@@ -0,0 +1,4 @@
+module purge
+module load profile/base  autoload  cmake  profile/advanced gnu/8.3.0  gsl/2.5--intel--pe-xe-2018--binary
+#module load  profile/base  cmake/3.23.3    profile/advanced  blas/3.8.0--intel--pe-xe-2018--binary  fftw/3.3.7--intelmpi--2018--binary  \
+#  intel/pe-xe-2018--binary    gnu/8.3.0       intelmpi/2018--binary    autoload                                gsl/2.5--intel--pe-xe-2018--binary 
